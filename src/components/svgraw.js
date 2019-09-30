42321cc6da596569c990cc1a3f49b54b796416ef
Paulina Hetman <info@pehaa.com>
2019-09-30 07:40:22 +0200

    map to for loop

--- src/components/svgraw.js
+++ src/components/svgraw.js
@@ -19,22 +19,22 @@ const countPattern = (threadcount, palette) => {
     return { fill: "#000000", size: 0 }
   })
   if (array[0].indexOf("/") > -1) {
-    for (var i = 0; i < 2 * array.length - 2; i++) {
+    for (let i = 0; i < 2 * array.length - 2; i++) {
       const index = i < array.length - 1 ? i : 2 * array.length - 2 - i
       result.push(colCountArray[index])
     }
     if (total % 2) {
-      for (var i = 0; i < 2 * array.length - 2; i++) {
+      for (let i = 0; i < 2 * array.length - 2; i++) {
         result.push(result[i])
       }
     }
   } else {
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       const index = i
       result.push(colCountArray[index])
     }
     if (total % 2) {
-      for (var i = 0; i < array.length - 1; i++) {
+      for (let i = 0; i < array.length - 1; i++) {
         result.push(result[i])
       }
     }
@@ -46,21 +46,21 @@ const SvgRaw = props => {
   const tartanCount = countPattern(threadcount, palette)
 
   let position = 0
-  var positions = [0]
-  for (var i = 1; i < tartanCount.length + 1; i++) {
+  let positions = [0]
+  for (let i = 1; i < tartanCount.length + 1; i++) {
     position += tartanCount[i - 1].size
     positions.push(position)
   }
   const size = positions[positions.length - 1]
   let svg = `<svg viewBox="0 0 ${size} ${size}" width="${size}" height="${size}" x="0"  y="0" xmlns="http://www.w3.org/2000/svg">${SvgDefs()}<g id="horizStripes">`
 
-  tartanCount.map((el, index) => {
-    svg += `<rect fill="${el.fill}" height="${el.size}" width="100%" x="0" y="${positions[index]}"/>`
-  })
+  for (let i = 0; i < tartanCount.length; i++) {
+    svg += `<rect fill="${tartanCount[i].fill}" height="${tartanCount[i].size}" width="100%" x="0" y="${positions[i]}"/>`
+  }
   svg += `</g><g id="vertStripes" mask="url(#grating)">`
-  tartanCount.map((el, index) => {
-    svg += `<rect fill="${el.fill}" width="${el.size}" height="100%" y="0" x="${positions[index]}"/>`
-  })
+  for (let i = 0; i < tartanCount.length; i++) {
+    svg += `<rect fill="${tartanCount[i].fill}" width="${tartanCount[i].size}" height="100%" y="0" x="${positions[i]}"/>`
+  }
   svg += `</g></svg>`
   return { svg: svg, size: size }
 }
