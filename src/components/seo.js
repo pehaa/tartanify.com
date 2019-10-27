093e091d921851a1da4f678a323388aee28dc767
Paulina Hetman <info@pehaa.com>
2019-10-27 18:33:44 +0100

    favicons

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Helmet from "react-helmet"
 import useSiteMetadata from "../hooks/user-sitemetadata"
+import favicon16 from "../assets/favicon-16x16.png"
+import favicon32 from "../assets/favicon-32x32.png"
+import faviconappletouch from "../assets/apple-touch-icon.png"
 
 const SEO = ({ title, description, lang = "en" }) => {
   const site = useSiteMetadata()
@@ -14,6 +17,10 @@ const SEO = ({ title, description, lang = "en" }) => {
       title={seo.title}
       titleTemplate={`%s | ${site.title}`}
     >
+      <link rel="apple-touch-icon" sizes="180x180" href={faviconappletouch} />
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+      <link rel="manifest" href="https://tartanify.com/site.webmanifest" />
       <meta name="description" content={seo.description} />
       <meta property="og:url" content="https://tartanify.com" />
       <meta property="og:title" content={title} />
