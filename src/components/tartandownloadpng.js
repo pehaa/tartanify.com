a3b4fde41e456e7f468e97c04f6f5d1b9cfb7536
Paulina Hetman <info@pehaa.com>
2019-12-29 11:44:54 +0100

    use hooks

--- src/components/tartandownloadpng.js
+++ src/components/tartandownloadpng.js
@@ -1,35 +1,36 @@
-import React from "react"
-class TartanRawSvg extends React.Component {
-  componentDidMount() {
+import React, { useRef, useEffect } from "react"
+
+const TartanRawSvg = props => {
+  const aEl = useRef(null)
+  useEffect(() => {
     const canvas = document.createElement("canvas")
-    canvas.width = 2 * this.props.svg.size
-    canvas.height = 2 * this.props.svg.size
+    canvas.width = 2 * props.svg.size
+    canvas.height = 2 * props.svg.size
     const ctx = canvas.getContext("2d")
     ctx.scale(2, 2)
     let img = new Image()
-    img.src = `data:image/svg+xml, ${encodeURIComponent(this.props.svg.svg)}`
+    img.src = `data:image/svg+xml, ${encodeURIComponent(props.svg.svg)}`
     img.onload = () => {
       ctx.drawImage(img, 0, 0)
-      this.ref.setAttribute("href", canvas.toDataURL("image/png"))
+      aEl.current.setAttribute("href", canvas.toDataURL("image/png"))
     }
-  }
-  render() {
-    return (
-      <a
-        ref={el => (this.ref = el)}
-        id="png-download"
-        download={`${this.props.name}.png`}
-        href-lang="image/png"
-      >
-        <span className="icon">&rsaquo;</span>{" "}
-        <span className="download-text">
-          <span className="hide-sm">Download as </span>PNG
-        </span>
-        <small className="hide-sm">{`(${2 * this.props.svg.size}x${2 *
-          this.props.svg.size}px)`}</small>
-      </a>
-    )
-  }
+  }, [aEl])
+  return (
+    <a
+      id={props.svg.name}
+      ref={aEl}
+      //id="png-download"
+      download={`${props.name}.png`}
+      href-lang="image/png"
+    >
+      <span className="icon">&rsaquo;</span>{" "}
+      <span className="download-text">
+        <span className="hide-sm">Download as </span>PNG
+      </span>
+      <small className="hide-sm">{`(${2 * props.svg.size}x${2 *
+        props.svg.size}px)`}</small>
+    </a>
+  )
 }
 
 export default TartanRawSvg
