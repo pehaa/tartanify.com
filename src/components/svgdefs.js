de881b31bd6a2c5584eced16c972b3e0837ecd73
PeHaa <info@pehaa.com>
2020-01-20 07:56:18 +0100

    more lisible

--- src/components/svgdefs.js
+++ src/components/svgdefs.js
@@ -1,15 +1,11 @@
-const SvgDefs = () => {
-  const multiplier = 4
-
-  const defs = `<defs>
-<pattern id="pattern" x="0" y="0" width="${2 * multiplier}" height="${2 *
-    multiplier}" patternUnits="userSpaceOnUse"><polygon points="0,${multiplier} 0,${2 *
-    multiplier} ${2 *
-    multiplier},0 ${multiplier},0" fill="#ffffff"/><polygon points="${multiplier},${2 *
-    multiplier} ${2 * multiplier},${2 * multiplier} ${2 *
-    multiplier},${multiplier}" fill="#ffffff"/></pattern><mask id="grating" x="0" y="0" width="1" height="1">  
-  <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern)"/></mask></defs>`
-  return defs
-}
+const SvgDefs = () => `<defs>
+  <pattern id="pattern" x="0" y="0" width="8" height="8" patternUnits="userSpaceOnUse">
+    <polygon points="0,4 0,8 8,0 4,0" fill="#ffffff"/>
+    <polygon points="4,8 8,8 8,4" fill="#ffffff"/>
+  </pattern>
+  <mask id="grating" x="0" y="0" width="1" height="1">
+    <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern)"/>
+    </mask>
+  </defs>`
 
 export default SvgDefs
