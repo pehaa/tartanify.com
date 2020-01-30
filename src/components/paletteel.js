2fba087bca28733196ebb5168b942494320e9737
Paulina Hetman <info@pehaa.com>
2020-01-30 09:59:08 +0100

    == to ===

--- src/components/paletteel.js
+++ src/components/paletteel.js
@@ -1,16 +1,17 @@
 import React from "react"
 
 const PaletteEl = ({ colors, id }) => {
+  // https://css-tricks.com/converting-color-spaces-in-javascript/
   const hue = H => {
     // Convert hex to RGB first
     let r = 0,
       g = 0,
       b = 0
-    if (H.length == 4) {
+    if (H.length === 4) {
       r = "0x" + H[1] + H[1]
       g = "0x" + H[2] + H[2]
       b = "0x" + H[3] + H[3]
-    } else if (H.length == 7) {
+    } else if (H.length === 7) {
       r = "0x" + H[1] + H[2]
       g = "0x" + H[3] + H[4]
       b = "0x" + H[5] + H[6]
